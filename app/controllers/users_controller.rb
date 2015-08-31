9093c95efc69c6bdef696974907e2e919e9216c7
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-08-31 11:17:32 -0400

    MVC for user sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    new_user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  def show
+    actual_user = User.find(session[:user_id])
+    if logged_in? && check_current_user?
+      @current_user = User.find(session[:user_id])
+      @events = @current_user.events
+    else
+      redirect_to user_path(actual_user)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
